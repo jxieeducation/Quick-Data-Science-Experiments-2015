cf20a9dedddcbe177a8e151ccedfea7eb4193fd6
Jason <jxieeducation@berkeley.edu>
2015-12-04 00:09:06 -0800

    added more feature engineering stuff

---
+++ kaggle_rossman/training/xgb_train.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+from sklearn.cross_validation import train_test_split
+import xgboost as xgb
+import operator
+import matplotlib
+matplotlib.use("Agg") #Needed to save figures
+import matplotlib.pyplot as plt
+
+X = np.load(open('../data/X_pickle'))
+y = np.load(open('../data/y_pickle'))
+test = np.load(open('../data/test_pickle'))
+features = np.load(open('../data/features_pickle'))
+
+def create_feature_map(features):
+    outfile = open('xgb.fmap', 'w')
+    for i, feat in enumerate(features):
+        outfile.write('{0}\t{1}\tq\n'.format(i, feat))
+    outfile.close()
+
+def rmspe(y, yhat):
+    return np.sqrt(np.mean((yhat/y-1) ** 2))
+
+def rmspe_xg(yhat, y):
+    y = np.expm1(y.get_label())
+    yhat = np.expm1(yhat)
+    return "rmspe", rmspe(y,yhat)
+
+params = {"objective": "reg:linear",
+          "booster" : "gbtree",
+          "eta": 0.3,
+          "max_depth": 10,
+          "subsample": 0.9,
+          "colsample_bytree": 0.7,
+          "silent": 1,
+          "seed": 1301
+          }
+num_boost_round = 1000
+
+print "features are: " + str(features)
+print("Train a XGBoost model")
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05, random_state=10)
+dtrain = xgb.DMatrix(X_train, y_train)
+dvalid = xgb.DMatrix(X_test, y_test)
+
+watchlist = [(dtrain, 'train'), (dvalid, 'eval')]
+gbm = xgb.train(params, dtrain, num_boost_round, evals=watchlist, \
+  early_stopping_rounds=50, feval=rmspe_xg, verbose_eval=True)
+
+print("Validating")
+yhat = gbm.predict(xgb.DMatrix(X_test))
+error = rmspe(y_test, np.expm1(yhat))
+print('RMSPE: {:.6f}'.format(error))
+
+create_feature_map(features)
+importance = gbm.get_fscore(fmap='xgb.fmap')
+importance = sorted(importance.items(), key=operator.itemgetter(1))
+df = pd.DataFrame(importance, columns=['feature', 'fscore'])
+df['fscore'] = df['fscore'] / df['fscore'].sum()
+print df
+
+featp = df.plot(kind='barh', x='feature', y='fscore', legend=False, figsize=(6, 10))
+plt.title('XGBoost Feature Importance')
+plt.xlabel('relative importance')
+fig_featp = featp.get_figure()
+fig_featp.savefig('feature_importance_xgb.png', bbox_inches='tight', pad_inches=1)
