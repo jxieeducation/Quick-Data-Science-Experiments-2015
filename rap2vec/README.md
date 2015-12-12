5d4ac591d9c20fe362c426ae3d1bf9ec7b86911f
Jason <jxieeducation@berkeley.edu>
2015-12-11 21:09:20 -0800

    added rap stuff

---
+++ rap2vec/README.md
@@ -0,0 +1,1 @@
+https://www.reddit.com/r/MachineLearning/comments/3wbbs6/rap2vec_finding_good_rhymes/
--- rap2vec/train.py
+++ rap2vec/train.py
@@ -1,8 +1,6 @@
 import numpy as np
 from gensim.models import Word2Vec
-import sys
-from os import listdir
-from os.path import isfile, join
+
 
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
@@ -10,17 +8,9 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 SEED = 1337
 np.random.seed(SEED)
 
-datadir = "data/cleaned-rap-lyrics/"
-files = [join(datadir, f) for f in listdir(datadir) if isfile(join(datadir, f)) and ".txt" in f]
-training_set = []
-for rap_file in files:
-	rap_file = open(rap_file)
-	lines = rap_file.read().split('\n')
-	cleaned_lines = [line.split(' ') for line in lines]
-	training_set += cleaned_lines
 
-print "finished loading corpus in memory"
+# print "finished loading corpus in memory"
 
-model = Word2Vec(training_set, min_count=1)
+# model = Word2Vec(training_set, min_count=1)
 
-model.save('data/rap.model')
+# model.save('data/rap.model')
